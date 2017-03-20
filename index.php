e30c567ea708f5e8f4e3258f6ac789c0d7c3e436
zelloufi <zelloufi.ilyas@gmail.com>
2017-03-20 16:54:21 +0300

    finish index actu

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php header( 'Location: /intercalaires_index.html' ) ;  ?>
+<h2>Voici la liste des pages</h2>
+<div>
+<a href="/intercalaires_index.html">Liste des intercalaires</a>
+</div>
+<div>
+<a href="/actualites.html">Liste des actualitées</a>
+</div>
